23206b055b1e81595436e39ecca140436be45287
RONALD KIMELI <48482772+KimelirR@users.noreply.github.com>
2022-07-26 16:03:26 +0300

    Update new3.php

--- new3.php
+++ new3.php
@@ -77,7 +77,6 @@
                     }
                 endforeach;           
             }
-
         ?>
 	</table>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
